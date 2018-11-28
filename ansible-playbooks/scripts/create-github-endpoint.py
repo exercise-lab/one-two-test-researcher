d354d81a04842483163cae259d8884c31dc37739
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-11-28 10:47:32 -0600

    Debugging run.py

--- ansible-playbooks/scripts/create-github-endpoint.py
+++ ansible-playbooks/scripts/create-github-endpoint.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import git
 import github3
 
@@ -5,12 +6,13 @@ import github3
 def create_github_repo(repo):
     github = github3.login(os.environ["GITHUB_USERNAME"], os.environ["GITHUB_PASSWORD"])
     org = github.organization("exercise-lab")
-    org.create_repo(repo)
+    org.create_repository(repo, private=True)
 
 
 def set_url(dest, repo_name):
     repo = git.Repo(dest)
-    repo.remotes.origin.set_url(f"git@github.com:exercise-lab/{repo_name}")
+    url = f"git@github.com:exercise-lab/{repo_name}"
+    repo.remotes.origin.set_url(url)
 
 
 if __name__ == "__main__":
@@ -20,4 +22,4 @@ if __name__ == "__main__":
     parser.add_argument("--repo", "-r")
     args = parser.parse_args()
     create_github_repo(args.repo)
-    set_origin(args.dest, args.repo)
+    set_url(args.dest, args.repo)
